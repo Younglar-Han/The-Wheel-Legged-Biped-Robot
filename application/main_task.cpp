ed372a3b28c7f90db087184f11a36d7492dc47a5
Younglar-Han <3369883232@qq.com>
2025-03-31 15:59:44 +0800

    feat: add time tick in main task

--- application/main_task.cpp
+++ application/main_task.cpp
@@ -2,15 +2,19 @@
 #include "cmsis_os.h"
 #include "main.h"
 
+#include "Time.hpp"
 #include "Dr16.hpp"
 
 Dr16 *pDr16 = Dr16::Instance();
+uint32_t time;
 
 void main_task(void const * argument)
 {
     Dr16::Instance()->Init();
     while(1)
     {
+        Time::Tick();
+        time = Time::GetTick();
         Dr16::Instance()->Update();
         osDelay(1);
     }
